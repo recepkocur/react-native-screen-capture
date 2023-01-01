2f1e186b3e3ec9e139c91f60c0a34153aba3e0aa
Recep Kocur <recepkocur@gmail.com>
2023-01-02 00:23:24 +0300

    Add userDidTakeScreenshot for only iOS

--- ios/ScreenCapture.m
+++ ios/ScreenCapture.m
@@ -1,20 +1,52 @@
 // ScreenCapture.m
 
+// #import <React/RCTLog.h>
+// RCTLogInfo(@"Example Log");
+
 #import "ScreenCapture.h"
 
 @implementation ScreenCapture {
+    BOOL hasListeners;
     BOOL dev;
     UITextField *secureTextField;
 }
 
 - (dispatch_queue_t)methodQueue {
-    dev = NO;
     if (dev) NSLog(@"[SC] methodQueue");
     return dispatch_get_main_queue();
 }
 
 + (BOOL)requiresMainQueueSetup {
     return YES;
+}
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"ScreenCapture_userDidTakeScreenshot"];
+}
+
+- (id)init {
+    dev = TRUE;
+    if (dev) NSLog(@"[SC] init");
+    if ((self = [super init])) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDidTakeScreenshot:) name:UIApplicationUserDidTakeScreenshotNotification object: nil];
+    }
+    return self;
+}
+
+- (void)startObserving {
+    if (dev) NSLog(@"[SC] startObserving");
+    hasListeners = YES;
+}
+
+- (void)stopObserving {
+    if (dev) NSLog(@"[SC] stopObserving");
+    hasListeners = NO;
+}
+
+- (void)userDidTakeScreenshot:(NSNotification *)notification {
+    if (!hasListeners) return;
+    if (dev) NSLog(@"[SC] userDidTakeScreenshot");
+    [self sendEventWithName:@"ScreenCapture_userDidTakeScreenshot" body:nil];
 }
 
 #pragma mark - dealloc -
@@ -39,15 +71,33 @@ RCT_EXPORT_METHOD(keepAwake:(BOOL)status)
 RCT_EXPORT_METHOD(disallowScreenshot:(BOOL)status)
 {
     if (dev) NSLog(@"[SC] disallowScreenshot with %@", status ? @"YES" : @"NO");
-    if (self->secureTextField == nil) {
-        self->secureTextField = [[UITextField alloc] init];
-        self->secureTextField.userInteractionEnabled = false;
-        UIWindow *window = [[[UIApplication sharedApplication] delegate] window];
-        [window addSubview:self->secureTextField];
-        [window.layer.superlayer addSublayer:self->secureTextField.layer];
-        [self->secureTextField.layer.sublayers.firstObject addSublayer:window.layer];
+    @try {
+        if (dev) NSLog(@"[SC] try");
+        if (self->secureTextField == nil) {
+            self->secureTextField = [[UITextField alloc] init];
+            self->secureTextField.userInteractionEnabled = false;
+            UIWindow *window = [[[UIApplication sharedApplication] delegate] window];
+            [window addSubview:self->secureTextField];
+            [window.layer.superlayer addSublayer:self->secureTextField.layer];
+            [self->secureTextField.layer.sublayers.firstObject addSublayer:window.layer];
+        }
+        [self->secureTextField setSecureTextEntry:status];
     }
-    [self->secureTextField setSecureTextEntry:status];
+    @catch (NSException *e) {
+        if (dev) NSLog(@"[SC] catch");
+    }
+}
+
+#pragma mark - reactnative samples -
+
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(getDeviceName) {
+    if (dev) NSLog(@"[SC] getDeviceName");
+    return [[UIDevice currentDevice] name];
+}
+
+RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback) {
+    if (dev) NSLog(@"[SC] sampleMethod");
+    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
 }
 
 @end
